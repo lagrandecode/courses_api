9e7f151027153b7926d31cbec845ab49311ee86b
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-23 00:58:50 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# '''creating class '''
+
+class Api(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    img = models.ImageField()
+    desc = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
