93f08a4099af2896a5487ad2d2034a8566405304
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-23 13:29:03 +0100

    "commit"

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,9 @@
+from pyexpat import model
 from rest_framework import serializers
+from api.models import Api
+
+
+class ApiSerializers(serializers.ModelSerializer):
+    class Meta: 
+        model = Api
+        fields = '__all__'
---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('api/',views.api_list),
+    path('api/<int:pk>', views.apidetail)
+]
+
+
