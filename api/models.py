b813303fad1bdfa04fc2799d821a478ee6417ecc
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-23 11:39:39 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -7,7 +9,7 @@ from django.db import models
 class Api(models.Model):
     name = models.CharField(max_length=50)
     url = models.URLField()
-    img = models.ImageField()
+    img = models.ImageField(default=True)
     desc = models.CharField(max_length=250)
 
     def __str__(self):
